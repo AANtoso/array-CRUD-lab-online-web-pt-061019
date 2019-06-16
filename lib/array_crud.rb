43a4f08ac1012ec9cc57683803fb800bf019f572
Adjoa Ntoso <adjoaan@gmail.com>
2019-06-16 13:55:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,5 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array
-
+  cute_dogs = ["wow", "I", "am", "really", "learning", "arrays!"]
+  cute_dogs.last
 end
